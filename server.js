d9aa9723e4be8e1910fd2395874af620458a2aac
kiokomutunga <kiokomutungapp@gmail.com>
2026-07-19 17:39:01 +0300

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import app from "./src/app.js";
+import "dotenv/config"
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+    cors(
+        {
+            origin: process.env.Url,
+        }
+    )
+) //only allow requests from this frontend
+
+app.get("/api/health", (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "Payment API is running",
+    environment: process.env.NODE_ENV,
+  });
+});
 
 app.get ("/", (response,request)=>{
     response.json(
@@ -11,10 +28,6 @@ app.get ("/", (response,request)=>{
             message: "Pay App"
         }
     )
-
-
-
-
 })
 
 export default app;
